5d150f2c301346102ae585a43980cac5925588de
unknown <komitmendirisendiri@gmail.com>
2025-05-15 22:20:22 +0700

    Tugas

---
+++ Tugas Pertemuan 7/problem2.js
@@ -0,0 +1,6 @@
+let T = 20;
+let r = 4;
+
+let luas = 2 * 3.14 * r * (r + T);
+
+console.log(luas);
---
+++ Tugas Pertemuan 7/problem3.js
@@ -0,0 +1,8 @@
+let panjang = 20;
+let lebar = 5;
+
+let luas = panjang * lebar;
+console.log("Luas Persegi Panjang = " + luas);
+
+let keliling = 2 * (panjang + lebar);
+console.log("Keliling Persegi Panjang = " + keliling);
